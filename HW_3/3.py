c23e8b24404ad4a99161e01edc5092d90b35dc4f
naabokova <naabokova@gmail.com>
2025-07-31 00:13:54 +0300

    git

--- HW_3/3.py
+++ HW_3/3.py
@@ -7,11 +7,13 @@
 # (сумма значений элементов, деленная на их количество)
 
 
-class Class:
+class NewBornList:
     def __init__(self, list_numbers = None):
         self.list_numbers = []
+
         if list_numbers is None:
             list_numbers = []
+
         for i in list_numbers:
             if isinstance(i, (int, float)):
                 self.list_numbers.append(i)
@@ -22,10 +24,13 @@ class Class:
     def aver(self):
         if not self.list_numbers:
             print("Нет чисел - нет списка")
+            return None
+        
         average = sum(self.list_numbers) / len(self.list_numbers)
         print(f"Среднее значение чисел списка: {average}")
+        
 
-new_list_nubmers = Class([1, 2, 3, "hi", 5, "whatsUp"])
+new_list_nubmers = NewBornList([1, 2, 3, "hi", 5, "whatsUp"])
 
 new_list_nubmers.show()
 new_list_nubmers.aver()
