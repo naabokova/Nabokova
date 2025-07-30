e85a40f038fb51d63509447d718ea0e102980856
naabokova <naabokova@gmail.com>
2025-07-31 00:11:49 +0300

    Done

---
+++ HW_3/3.py
@@ -0,0 +1,31 @@
+# Напишите программу, в которой описан класс. 
+# У объектов класса должно быть поле, представляющее собой числовой список. 
+# Этот список формируется на основе списка, переданного конструктору в качестве аргумента.
+# При этом из списка-аргумента в список-поле включаются только числовые элементы (элементы других типов игнорируются). 
+# Необходимо также описать метод, отображающий содержимое поля списка, 
+# а также метод, вычисляющий среднее значение элементов поля списка 
+# (сумма значений элементов, деленная на их количество)
+
+
+class Class:
+    def __init__(self, list_numbers = None):
+        self.list_numbers = []
+        if list_numbers is None:
+            list_numbers = []
+        for i in list_numbers:
+            if isinstance(i, (int, float)):
+                self.list_numbers.append(i)
+
+    def show(self):
+        print(f"Числовой список: {self.list_numbers}")
+
+    def aver(self):
+        if not self.list_numbers:
+            print("Нет чисел - нет списка")
+        average = sum(self.list_numbers) / len(self.list_numbers)
+        print(f"Среднее значение чисел списка: {average}")
+
+new_list_nubmers = Class([1, 2, 3, "hi", 5, "whatsUp"])
+
+new_list_nubmers.show()
+new_list_nubmers.aver()
